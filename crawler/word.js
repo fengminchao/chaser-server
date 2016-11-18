84e042206658e3a55aac8a005504cb4e1d654023
fengminchao <360322537@qq.com>
2016-11-18 16:25:37 +0800

    word crawler

---
+++ crawler/word.js
@@ -0,0 +1,39 @@
+var cheerio = require('cheerio');
+var request = require('superagent');
+// classId 为一个词库 id，courseId 为课程内每个章节的id
+exports.getCourse = function(classId,courseId){
+	request.get('http://word.iciba.com/?action=words&class=' + classId + '&course=' + courseId)
+	.send()
+	.end(function(err,res){
+		if (err) {
+			console.log(err);
+			return;
+		}
+		parseWordSchema(res.text);
+	})
+}
+
+function parseWordSchema(text){
+	var $ = cheerio.load(text);
+	var item = [];
+	$('.word_main_list li').each(function(id,element){
+		var word = $('.word_main_list_w span',element).text();
+		var phonetic = $('.word_main_list_y strong',element).text();
+		var explain = $('.word_main_list_s span',element).text();
+		//剔除字段中的转义符
+		word = word.replace(/\t+/g,'').replace(/\n+/g,'');
+		phonetic = phonetic.replace(/\t+/g,'').replace(/\n+/g,'');
+		explain = explain.replace(/\t+/g,'').replace(/\n+/g,'');
+		item.push({
+			word: word,
+			phonetic: phonetic,
+			explain: explain
+		});
+
+		// console.log(word + '\n' + phonetic + '\n' + explain);
+	});
+}
+
+for (var i = 1; i < 30; i ++) {
+	exports.getCourse(11,i);
+}
